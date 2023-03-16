3581efcd07489d37de95d4ff1ed15c0d3a117928
Zeusmallan <jaklemallan@yahoo.com>
2023-03-16 12:51:49 -0700

    Hello World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - A programmame that prints a line using the pritf function
+ * return: 0 (Success)
+ */
+
+int main(void)
+
+
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
