bc3b612197ff41c047c4c406fd9ed8764155be17
Zeusmallan <jaklemallan@yahoo.com>
2023-03-16 12:05:37 -0700

    C Hello, World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+/**
+ * main - This is a programming that prints or displays a line with the puts function
+ * Return: 0 (pass)
+ */
+
 int main (void)
 {
-	puts("Programming is lke a multilingual puzzle \n);
+	puts("\"Programming is lke a multilingual puzzle \n);
 	
 	return=0;
 }
