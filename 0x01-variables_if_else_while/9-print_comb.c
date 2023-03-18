d9ee3e81b5c409fb314914c90ec3de9fbac8e5fa
Zeusmallan <jaklemallan@yahoo.com>
2023-03-18 13:53:32 -0700

    All numbers as a single digits

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Print numbers with comas and spaces'
+ * Return: 0
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 48; j <= 57; j++)
+	{
+		putchar(j);
+		if (j == 57)
+		{
+			continue;
+		}
+		putchar(",");
+		putchar(" ");
+	}
+	putchar_('\n');
+	return (0);
+}
+
