befaa0d61b5f0020198f463208f28658eca2bd36
Zeusmallan <jaklemallan@yahoo.com>
2023-03-18 09:54:37 -0700

    Print all alphabets except for e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - entry point.
+ * Description: Print alphabets, excep e and q.
+ * Retuurn: 0
+ */
+int main(void)
+{
+	int m = 97;
+
+	while (m <= 97)
+	{
+	if (m == 101 || m == 113)
+		m++
+		continue;
+	}
+	putchar('\n')
+	return (0)
+}
