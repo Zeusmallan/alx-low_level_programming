3c2f6ee8ae0e532e367f5b095457e9999752bd35
Zeusmallan <jaklemallan@yahoo.com>
2023-03-18 12:47:44 -0700

    Printing all base 10 digits using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print all base 10 digits
+ * Return Always (0)
+ */
+int main(void)
+{
+	int m;
+
+	for (m = o; m <= 9; m++)
+	{
+		putchar(m + 48);
+	}
+	putchar('\n');
+	return (0);
+}
