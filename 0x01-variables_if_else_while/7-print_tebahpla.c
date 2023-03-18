a05add1265ebdc423bc1e242dc1a1bb708ff0835
Zeusmallan <jaklemallan@yahoo.com>
2023-03-18 13:09:38 -0700

    Alphabets in reverse order

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Prints alphabets backwards in lowercase'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int m = 122;
+
+	while (m >= 97)
+	{
+		putchar(m);
+		n--
+	}
+	putchar('\n')
+	return (0);
+}
