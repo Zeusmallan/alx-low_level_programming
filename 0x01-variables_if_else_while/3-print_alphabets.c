0bb0ba84f9d0e542cf0bf21efdbc2c01fe4e4b1d
Zeusmallan <jaklemallan@yahoo.com>
2023-03-18 07:19:23 -0700

    Lower case to upper case alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:Prints alphabets in lower case and then in upper case.
+ * Return: 0
+ */
+int main(void)
+{
+	int u = 97;
+	int l = 65;
+
+	while (u <= 122)
+	{
+	putchar(u);
+	u++;
+	}
+	while (l <= 90)
+	{
+	putchar(l);
+	l++;
+	}
+	putchar('\n')
+	return (0);
+}
