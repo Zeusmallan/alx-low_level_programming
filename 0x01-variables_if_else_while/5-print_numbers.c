0ae2af01a87d05361f91df43bfe20acb45e59827
Zeusmallan <jaklemallan@yahoo.com>
2023-03-18 11:53:22 -0700

    Print numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Print single numbers of base 10'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int m;
+
+	for (m == 0; n <= 9; m++)
+	{
+		printf("%d", m);
+	}
+	printf('\n');
+	return (0);
+}
+
