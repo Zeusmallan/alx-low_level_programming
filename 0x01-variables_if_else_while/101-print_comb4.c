93172f2bdc8508818561d54ba6cf39a52e3b9726
Zeusmallan <jaklemallan@yahoo.com>
2023-03-20 14:56:54 -0700

    Prints digits of three combinations

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+/**
+ * main - Entry Point
+ * Description: 'Prints all combination of three digits'
+ * Return: 0
+ */
+int main(void)
+{
+        int a = 0;
+        int b = 0;
+        int c = 0;
+
+        while (c < 10)
+        {
+                b = 0;
+                while (b < 10)
+                {
+                        a = 0;
+                        while (a < 10)
+                        {
+                                if (a != b && b != c && c < b && b < a)
+                                {
+                                        putchar('0' + c);
+                                        putchar('0' + b);
+                                        putchar('0' + a);
+                                }
+                        }
+                        a++;
+                }
+                b++
+        }
+        c++;
+        putchar('\n');
+        return (0);
+}
