56e9f7a754844581684bd0d4dcf347b47cfd77de
Zeusmallan <jaklemallan@yahoo.com>
2023-03-20 15:31:59 -0700

    Print three digits combinations

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,34 +1,40 @@
+#include <stdio.h>
 /**
  * main - Entry Point
- * Description: 'Prints all combination of three digits'
+ * Description: 'Print digits combinations'
  * Return: 0
  */
 int main(void)
 {
-        int a = 0;
-        int b = 0;
-        int c = 0;
+	int a;
+	int b;
+	int c = 0;
 
-        while (c < 10)
-        {
-                b = 0;
-                while (b < 10)
-                {
-                        a = 0;
-                        while (a < 10)
-                        {
-                                if (a != b && b != c && c < b && b < a)
-                                {
-                                        putchar('0' + c);
-                                        putchar('0' + b);
-                                        putchar('0' + a);
-                                }
-                        }
-                        a++;
-                }
-                b++;
-        }
-        c++;
-        putchar('\n');
-        return (0);
+	while (c < 10)
+	{
+		b = 0;
+		while (b < 10)
+		{
+			a = 0;
+			while (a < 10)
+			{
+				if (a != b && b != c && c < d && b < a)
+				{
+					putchar('0' + a);
+					putchar('0' + b);
+					putchar('0' + c);
+					if (c + d + e != 9 + 8 + 7)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+				a++;
+			}
+			b++
+		}
+		c++
+	}
+	putchar('\n');
+	return (0);
 }
