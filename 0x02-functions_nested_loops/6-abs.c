b7c9b4a2844e79f2664d24e1bc8423b6be15120c
Zeusmallan <jaklemallan@yahoo.com>
2023-03-21 15:25:45 -0700

    C functions and nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Prints the absolute value of an integer.
+ * @int: Integer or number.
+ * Return: absolute value.
+ */
+int _abs(int);
+{
+	if (i >= 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
