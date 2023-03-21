8014182f4d22cc1d389bb3d567543f58e0ed8daf
Zeusmallan <jaklemallan@yahoo.com>
2023-03-21 15:13:26 -0700

    C Functions and nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign - Prints the sign of a number.
+ * @n: Any interger.
+ * Return: 1 is greater than zero, 0 is zero -1 is less than zero.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(48);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
+
+
