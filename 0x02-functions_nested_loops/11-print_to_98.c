78972689f03f3ae6418540d1d7993079e8aaac00
Zeusmallan <jaklemallan@yahoo.com>
2023-03-22 13:49:13 -0700

    C function and nested loop

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,19 +10,15 @@ void print_to_98(int n)
 	if (n >= 99)
 	{
 		while (n < 98)
-		{
+		
 			printf("%d, ", n--);
 			printf("\n");
 		}
 		else
 		{
 			while (n < 98)
-			{
+			
 				printf("%d, ", n++);
 				printf("\n");
-			}
 		}
-	}
 }
-
-
