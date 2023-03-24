d90a63f46bef7879c4d0cb70b2eef6199e727fd9
Zeusmallan <jaklemallan@yahoo.com>
2023-03-24 06:58:13 -0700

    More C functions

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - A function that 0-9 without 2 and 4.
+ * Return: 0-9
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || ii == 4)
+			i++;
+		_putchar(i + '0');
+		_putchar('\n');
+	}
+}
