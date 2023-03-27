2306882d2478ae77361f268135985c9a917b6b22
Zeusmallan <jaklemallan@yahoo.com>
2023-03-27 10:58:15 -0700

    C arrays, srings and pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - Function that updates the value of an address to 98.
+ * @n: A pointer of a variable.
+ * Return; 0 (Success).
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is a project in The Alx programming school on C arrays, C pointers and Strings in C.
