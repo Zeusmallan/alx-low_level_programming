0fa10503891ba85c88701fa00a9908166aa7800f
Zeusmallan <jaklemallan@yahoo.com>
2023-03-27 12:17:38 -0700

    C strings, arrays and pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,11 +6,11 @@
  */
 void print_rev(char *s)
 {
-	int a = 0
+	int a = 0;
 
 	while (s[a] != '\0')
 		a++;
-		for (i = a - 1; a >= 0; a--)
+		for (a = a - 1; a >= 0; a--)
 			_putchar(s[a]);
 		_putchar('\n');
 }
