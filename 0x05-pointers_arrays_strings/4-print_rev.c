f53cd08b60aea9a81844ef6c3301a95bc9ac706d
Zeusmallan <jaklemallan@yahoo.com>
2023-03-27 13:31:31 -0700

    C arrays and pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,13 +8,9 @@ void print_rev(char *s)
 {
 	int a = 0;
 
-	while (*(s + a) != '\0')
-	{
-		a++;
-	}
-	for (int i = a - 1; a >= 0; a--)
-	{
+	while (s[a] != '\0')
+		i++;
+	for (a = a - 1; a >= 0; a--)
 		_putchar(s[a]);
-	}
-		_putchar('\n');
+	_putchar('\n');
 }
