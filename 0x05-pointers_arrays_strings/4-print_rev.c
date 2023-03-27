7625f14ba8bdbefebcedd48b1da2c0bd7d43e331
Zeusmallan <jaklemallan@yahoo.com>
2023-03-27 12:27:55 -0700

    C strings, arrays and pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,18 +1,20 @@
 #include "main.h"
 /**
  * print_rev - Function that prints string in reverse order.
- * @s Address of the string to be printed.
+ * @s: Address of the string to be printed.
  * Return: 0 (success).
  */
 void print_rev(char *s)
 {
 	int a = 0;
 
-	while (s[a] != '\0')
+	while (*(s + a) != '\0')
 	{
 		a++;
-		for (a = a - 1; a >= 0; a--)
-			_putchar(s[a]);
+	}
+	for (int i = a - 1; a >= 0; a--)
+	{
+		_putchar(s[a]);
 	}
 		_putchar('\n');
 }
