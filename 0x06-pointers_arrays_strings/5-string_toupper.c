4824b51483d08411167b0614bc08f84750f0174c
Zeusmallan <jaklemallan@yahoo.com>
2023-03-30 12:52:19 -0700

    More pointers and strings in C

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#inclue "main.h"
+/**
+ * string_toupper - Changes characters to uppper case
+ * @j: Pointer of a string.
+ * Return: 0 (Success)
+ */
+char *string_toupper(char *j)
+{
+	int m = 0;
+
+	while (j[m] != '\0')
+	{
+		if (j[m] >= 'a' && j[m] <= 'z')
+			j[m] = j[m] - 32;
+		m++
+	}
+	return (j);
+}
