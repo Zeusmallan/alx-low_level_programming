e2bb18b0136834d3ade3376fb9c9153cad3429d2
Zeusmallan <jaklemallan@yahoo.com>
2023-03-30 15:00:07 -0700

    Pointers and strings in C

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,24 @@
 #include "main.h"
 /**
  * leet - Function that encodes a string into 1337.
- * @p: String pointer
+ * @n: String pointer
  * Return: p value
  */
-char *leet(char *p)
+char *leet(char *n)
 {
 	int i, j;
 	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
-	for (i = 0; p(i) != '\0'; i++)
+	for (i = 0; n(i) != '\0'; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (p[i] == s1[j])
+			if (n[i] == s1[j])
 			{
-				p[i] = s2[j];
+				n[i] = s2[j];
 			}
 		}
 	}
-	return (p);
+	return (n);
 }
