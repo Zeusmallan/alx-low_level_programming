c99a3ed0cce94fc7aea387c080af4b55c02c2d33
Zeusmallan <jaklemallan@yahoo.com>
2023-03-30 14:01:07 -0700

    More pointers and strings in C

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,34 @@
 #include "main.h"
 /**
  * cap_string - Capitalizes all the words in a string.
- * @n: Pointer.
+ * @n: Pointer of the string to be capitalized.
  * Return: n
  */
+char *cap_string(char *n)
+{
+	int index = 0;
+
+	while (n[index])
+	{
+		while (!(n[inex] >= 'a' && n[index] <= 'z'))
+			index++;
+		if (n[index - 1] == ' ' ||
+				n[index - 1] == '\t' ||
+				n[index - 1] == '\n' ||
+				n[index - 1] == ',' ||
+				n[index - 1] == ';' ||
+				n[index - 1] == '_' ||
+				n[index - 1] == '!' ||
+				n[index - 1] == '?' ||
+				n[index - 1] == '"' ||
+				n[index - 1] == '(' ||
+				n[index - 1] == ')' ||
+				n[index - 1] == '{' ||
+				n[index - 1] == '}' ||
+				index == 0)
+			n[index] -= 32;
+		index++;
+	}
+	return (n);
+}
 
