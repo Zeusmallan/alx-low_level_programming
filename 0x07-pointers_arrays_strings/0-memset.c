679d5bc0cd8ea8e5100cd20fdc1e66e2122bff85
Zeusmallan <jaklemallan@yahoo.com>
2023-04-03 04:47:38 -0700

    Even more pointers and strings in C

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - A function that fills memory with constant byte.
+ * @s: A pointer of a string.
+ * @b: Variable of type char.
+ * @n: Variable of integer type.
+ * Return: Pointer to the memory location of S.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int j = 0;
+
+	for (; n > 0; j++)
+	{
+		S(J) = b;
+		n--;
+	}
+	return (s);
+}
