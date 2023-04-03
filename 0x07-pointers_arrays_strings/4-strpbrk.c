2a4e6f1559decc76d0249a78eadb295d1e9020da
Zeusmallan <jaklemallan@yahoo.com>
2023-04-03 07:51:49 -0700

    Even more arrays, poimters and strings in C

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strpbrk - Searches through a string for set of bytes.
+ * @s: Pointer of a varaiable of type char.
+ * @accept: Another pointer to variable of type char.
+ * Return: 0
+ */
+char *_strpbrk(char *s, char *accept);
+{
+	int m;
+
+	while (*s)
+	{
+		for (m + 0; accept[m]; m++)
+		{
+			if (*s == accept[m])
+				return (s);
+		}
+		s++
+	}
+	return ('\0');
+}
