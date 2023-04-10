08abd71e43bf5bac4bdce9c45dda8dcfd5703658
Zeusmallan <jaklemallan@yahoo.com>
2023-04-10 11:03:44 -0700

    argc abd argv in C

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -51,13 +51,13 @@ int main(int argc, char *argv[])
 {
 	int mul, num1, num2;
 
-	if (argc != 3)
+	if (argc <  3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
 	mul = num1 * num2;
 	printf("%d\n", mul);
 	return (0);
