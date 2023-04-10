2712613ee32dd4f6f4de430f2e48fdc13cee43e1
Zeusmallan <jaklemallan@yahoo.com>
2023-04-10 09:53:20 -0700

    argc and argv in C

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio>
+#include "main.h"
+/**
+ * main - Prints all arguements passed.
+ * @argc: Number of arguments.
+ * @argv: Array of arguments.
+ * Return: 0 (SUCCESSFUL).
+ */
+int main(int argc, char *argv[])
+{
+	int n;
+
+	for (n = 0; n < argc; n++)
+	{
+		printf("%s\n", argv[n]);
+	}
+	return (0);
+}
