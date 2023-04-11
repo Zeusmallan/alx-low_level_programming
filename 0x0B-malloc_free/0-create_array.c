5ad88917cee6ea3ce3bf87db3b7e9cbf8e6639ca
Zeusmallan <jaklemallan@yahoo.com>
2023-04-11 06:57:58 -0700

    Dynamic memory allocation in C

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,9 +1,9 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * create_array - Creates an array of char and 
+ * create_array - Creates an array of char and
  * initializes it with a specific char
- * @unsigned int size: Size of the array.
+ * @size: Size of the array.
  * @c: Character to initialize the array to.
  * Return: Pointer to the array, if there is any, return NULL.
  */
@@ -11,6 +11,7 @@ char *create_array(unsigned int size, char c)
 {
 	char *string;
 	unsigned int n;
+
 	string = malloc(sizeof(char) * size);
 	if (size == 0 || string == NULL)
 	{
