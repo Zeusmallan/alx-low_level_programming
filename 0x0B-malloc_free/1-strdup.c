3836095497e3428a66c4229ec141400432eed609
Zeusmallan <jaklemallan@yahoo.com>
2023-04-11 11:37:22 -0700

    Dynamic memory allocation in C

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -46,14 +46,14 @@ char *_strdup(char *str)
 	char *dest;
 	unsigned int size;
 
-	if (str == '\0')
+	if (str == 0)
 	{
 		return (NULL);
 	}
 	size = strlen[str] + 1;
 
 	dest = (char *) malloc(size * sizeof(char));
-	if dest = '\0'
+	if (dest = 0)
 	{
 		return (NULL);
 	}
