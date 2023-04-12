8936ad49f969baa999b77927a44fa6efac90c18d
Zeusmallan <jaklemallan@yahoo.com>
2023-04-11 21:17:21 -0700

    Dynamic memory allocation in C

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,4 +1,18 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * free_grid - 
+ * free_grid - allocates and free memory.
+ * @grid: Width of a grid.
+ * @height: Height of a grid.
+ * Return: Free or empty grid.
+ */
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	for (a = 0; a < height; a++)
+	{
+		free(grid[a]);
+	}
+	free(grid);
+}
