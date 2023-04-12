19f6cf8f7f03fdc217da837339ba5b978db41e13
Zeusmallan <jaklemallan@yahoo.com>
2023-04-11 21:02:42 -0700

    Dynamic memory allocation in C

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include <stlib.h>
+#include "main.h"
+/**
+ * alloc_grid - Allocates memory to a 2d array.
+ * @width: One of the dimensions of array.
+ * @height: Another dimension of the array.
+ * Return: Pointer to the memody grid.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int a, b;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(sizeof(int *) * height);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (a = 0; a < height; a++)
+	{
+		ptr[a] = malloc(sizeof(int *) * width);
+		if (ptr[a] == NULL)
+		{
+			for (a = a - 1; a >= 0; a--)
+			{
+				free(ptr);
+				return (NULL);
+			}
+		}
+		for (a = 0; b < width; b++)
+			ptr[a][b] = 0;
+		return (ptr);
+	}
+}
