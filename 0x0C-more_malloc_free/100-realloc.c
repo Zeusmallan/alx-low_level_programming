5b06422d8c14c3ade4d106669e8280c5064fae61
Zeusmallan <jaklemallan@yahoo.com>
2023-04-14 01:16:18 -0700

    More alloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -27,7 +27,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (new_size == 0 && ptr != NULL)
 	{
-		fre(ptr);
+		free(ptr);
 		return (0);
 	}
 	realloc = malloc(new_size);
