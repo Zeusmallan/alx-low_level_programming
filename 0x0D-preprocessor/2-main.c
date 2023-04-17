b5fccdb09650b33e782075dac30bec4e048575e5
Zeusmallan <jaklemallan@yahoo.com>
2023-04-17 12:37:54 -0700

    Preprocessor in C

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Main function that prints the name of the file 
+ * a programme was compiled from.
+ * Return: 0 (Success).
+ */
+void main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0)
+}
