37e7b019922dcc840398b563046cbeb771f2fd4f
Zeusmallan <jaklemallan@yahoo.com>
2023-04-19 12:35:45 -0700

    Function pointers in C

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-#include "main.h"
+#include "function_pointers.h"
+/**
+ * print_name - Function that prints a name.
+ * @name: Pointer to a string of characters.
+ * @f: Pointer to a function
+ * Return: Void.
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
