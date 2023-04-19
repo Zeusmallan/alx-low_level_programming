806f3517e98960983f63b704ae371772d4bf2da0
Zeusmallan <jaklemallan@yahoo.com>
2023-04-18 19:23:02 -0700

    Struct and typedef in C

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This is a directory that deals with structures and typedef in C programming language. This is a project in the ALX software enginerring programming
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Data about dog
+ * @name: Name of dog
+ * @age: age of dog
+ * @owner: owner of the dog.
+ * Description: This is a structure for do.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_d;
+void info_dog(struct dog *d, char *name, float age, chare *owner);
+void show_dog(struct dog *d);
+dog_d *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif /*DOG_H*/
