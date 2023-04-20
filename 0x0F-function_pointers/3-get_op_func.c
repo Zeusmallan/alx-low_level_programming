dbdc7018c30033cd3c5e9f3d32d76aa797db90f8
Zeusmallan <jaklemallan@yahoo.com>
2023-04-19 21:22:50 -0700

    Function Pointers in C

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,19 @@
+#include "3-calc.h"
+#include <stdllib.h>
+/**
+ * get_op_func - Allows the user tp select an operation to perform.
+ * @s: The opreator paseed as argument.
+ * Return: Pointer to the function that correlates with the operator
+ * passed as a parameter.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL},
+	};
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,60 @@
+#include "3-calc.h"
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+/**
+ * op_add - Adds two numbers.
+ * @a: First Number.
+ * @b: Second Number.
+ * Return: Sum of a and b.
+ */
+int op_add(int a, int b);
+{
+	return (a + b);
+}
+/**
+ * op_sub - Subtracts two numbers.
+ * @a: First Number.
+ * @b: Second Number.
+ * Return: Difference of a and b.
+ */
+int op_sub(int a, int b);
+{
+	return (a - b);
+}
+/**
+ * op_mul - Multiplies two numbers.
+ * @a: First Number.
+ * @b: Second Number.
+ * Return: Product of a and b.
+ */
+int op_mul(int a, int b);
+{
+	return (a * b);
+}
+/**
+ * op_div - Divides two numbers.
+ * @a: First Number.
+ * @b: Second Number.
+ * Return: Dision of a and b.
+ */
+int op_div(int a, int b);
+{
+	return (a / b);
+}
+/**
+ * op_mod - Gives remainder of the division of two numbers.
+ * @a: First Number.
+ * @b: Second Number.
+ * Return: Remainder of the division of a and b.
+ */
+int op_mod(int a, int b);
+{
+	return (a % b);
+}
+
+
+
