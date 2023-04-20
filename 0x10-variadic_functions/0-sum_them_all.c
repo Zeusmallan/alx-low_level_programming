605b2f4ec12a36fcf939cc1bb8af1bc2e745a401
Zeusmallan <jaklemallan@yahoo.com>
2023-04-20 12:31:12 -0700

    Variadic functions in C

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - Variadic function that sums all the parameters.
+ * @n: Parameters passed to the function.
+ * @...: All the parameters passed as arguments.
+ * Return: -0 if n = 0. otherwise the sum of the parameters.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	unsigned int sum = 0;
+
+	va_start(args, n);
+	for (i = 0, i < n; i++)
+	{
+		sum += va_arg(args, int);
+		va_end(args);
+	}
+	return (sum);
+}
+
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+This directory is about Variadic functions in the ALX software enginerring programme.
