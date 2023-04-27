bc7ec19b68e632c1572e52fecdf7ffcdb03d1cd2
Zeusmallan <jaklemallan@yahoo.com>
2023-04-27 04:40:46 -0700

    Singly linked lists in C

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,10 +18,10 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%u]%s\n", h->len, h->str);
+			printf("[%u] %s\n", h->len, h->str);
 		}
-			h = h->next;
-			s++;
+		h = h->next;
+		s++;
 	}
 	return (s);
 }
