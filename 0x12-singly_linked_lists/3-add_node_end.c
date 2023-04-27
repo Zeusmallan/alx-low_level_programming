be0fce51d589f5f5a3b820fec1b4ab75a6abef3b
Zeusmallan <jaklemallan@yahoo.com>
2023-04-27 06:45:11 -0700

    Singly linked lists in C

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node_end - Function that adds a node at the end of a list.
+ * @head: Pointer of the first node.
+ * @str: String to be added as the node at the end of the list.
+ * Return: Address of the new element and NULL if it fails.
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *temp = *head;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+	new = malloc(sizeof(list_t));
+	if (!new)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new;
+	return (new);
+}
