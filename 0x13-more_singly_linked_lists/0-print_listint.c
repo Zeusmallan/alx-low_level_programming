f22c6f74848a67dbe8b795e70d72824f63df397a
Zeusmallan <jaklemallan@yahoo.com>
2023-04-28 03:23:44 -0700

    More singly linked lists in C

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint - Function that prints all the elements of a singly linked list
+ * @h: Pointer to the first element of the linked list.
+ * Return: The number of nodes in the linked list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t elements = 0;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		elements++;
+	}
+	return (elements);
+}
---
+++ 0x13-more_singly_linked_lists/READEME.md
@@ -0,0 +1,1 @@
+This directory is about the more linked list project in the Alx software enginerring programme.
