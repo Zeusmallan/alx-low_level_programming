aded2b27cbb0a879a379947b71d5a0fe3ab0af48
Zeusmallan <jaklemallan@yahoo.com>
2023-04-29 16:36:30 -0700

    More singly linked lists in C

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_listint2 - Functions that frees all the nodes,
+ * except for the first one and turns the pointer to null.
+ * @head: Pointer to the first node.
+ * Return: 0 (Success).
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *present;
+	listint_t *next;
+
+	if (*head == NULL)
+	{
+		printf("Error:There is only one node with NULL pointer already\n");
+	}
+	present = *head;
+	{
+		while (present != NULL)
+		{
+			next = present->next;
+			free(present);
+			present = next;
+		}
+		*head = NULL;
+	}
+}
