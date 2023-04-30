ce5f9ffccb5f1eda80878dc869d9613f5dab6c60
Zeusmallan <jaklemallan@yahoo.com>
2023-04-30 03:35:32 -0700

    Updated my lists.h file

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,11 +9,19 @@ size_t print_listint(const listint_t *h)
 {
 	size_t elements = 0;
 
-	while (h)
+	if (h == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 		elements++;
 	}
 	return (elements);
+	}
 }
