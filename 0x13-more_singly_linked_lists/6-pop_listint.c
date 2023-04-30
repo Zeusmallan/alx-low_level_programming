8fcb8c9f3c1066076299e9af2b011cdbd85cb038
Zeusmallan <jaklemallan@yahoo.com>
2023-04-30 02:48:50 -0700

    More singly linked lists in C

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * pop_listint - Function that deletes the first node of a list.
+ * @head: Pointer that points to the pointer of the first node.
+ * Return: 0 if the Node is empty and 
+ * the pointer to the new node upon completion.
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *present;
+	int data_in_node;
+
+	if (*head == NULL)
+	{
+		printf("There is nothing to delete, the list is empty\n");
+		return (0);
+	}
+	else
+	{
+		present = *head;
+		*head = (*head)->next;
+		data_in_node = present->n;
+		free(present);
+	}
+	return (data_in_node);
+}
